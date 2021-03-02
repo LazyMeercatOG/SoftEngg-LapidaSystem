dac02930c27a5edcb8d3a1a989711923dc308b24
JRiesgo-05 <jerimiahriesgo@bizkittechnologies.com>
2021-03-02 20:56:02 +0800

    ADDED UID FOREIGN KEY AND MASTERDATA DB

--- lapida/lapida_app/admin.py
+++ lapida/lapida_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Profile, User_Place
+from .models import Profile, User_Place, MasterData
 
 
 admin.site.register(Profile)
 admin.site.register(User_Place)
-
+admin.site.register(MasterData)
 # Register your models here.
--- lapida/lapida_app/models.py
+++ lapida/lapida_app/models.py
@@ -20,8 +20,9 @@ class Profile(models.Model):
 	def __str__(self):
 		return f'{self.user.username} Profile'
 
-class User_Place(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+class MasterData(models.Model):
+	uid = models.CharField(primary_key=True,max_length=50)
+	place = models.CharField(max_length=60)
 	last_name = models.CharField(max_length=60)
 	first_name = models.CharField(max_length=60)
 	middle_name = models.CharField(max_length=60)
@@ -30,4 +31,20 @@ class User_Place(models.Model):
 	street = models.CharField(max_length=12)
 	lot = models.CharField(max_length=3)
 	def __str__(self):
-		return f'{self.user.username} User_Place'
+		return f'{self.uid}'
+
+
+class User_Place(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	uid = models.ForeignKey(MasterData,on_delete=models.CASCADE,default="hIP1ToECJxcUfazI")
+	last_name = models.CharField(max_length=60)
+	first_name = models.CharField(max_length=60)
+	middle_name = models.CharField(max_length=60)
+	category = models.CharField(max_length=2, choices=STORAGE_CHOICES) 
+	blk = models.CharField(max_length=3)
+	street = models.CharField(max_length=12)
+	lot = models.CharField(max_length=3)
+	def __str__(self):
+		return f'{self.last_name}{self.first_name}{self.middle_name}'
+
+
